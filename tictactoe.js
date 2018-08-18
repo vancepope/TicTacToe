632c94d9ea9ef23572c934984162d3fc9797117d
Vance Pope <vancepope@outlook.com>
2018-08-18 09:01:28 -0700

    second commit: 08/18/2018

--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,8 @@
-var winner = new Array();
-		var humanPickedArray = new Array();
-		var cpuPickedArray = new Array();
-		var cpuRepickArray = new Array();
-		var humanWinArray, cpuWinArray;
+	var gameWinner = new Array();
+	var humanPickedArray = new Array();
+	var cpuPickedArray = new Array();
+	var cpuRepickArray = new Array();
+	var humanWinArray, cpuWinArray, resetButton;
 		var winningCombos = [
     							"#1.COLOR, #2.COLOR, #3.COLOR", "#3.COLOR, #2.COLOR, #1.COLOR", "#2.COLOR, #1.COLOR, #3.COLOR", "#1.COLOR, #3.COLOR, #2.COLOR",
 								"#4.COLOR, #5.COLOR, #6.COLOR", "#6.COLOR, #5.COLOR, #4.COLOR", "#5.COLOR, #4.COLOR, #6.COLOR", "#6.COLOR, #4.COLOR, #5.COLOR",
@@ -17,6 +17,7 @@ var winner = new Array();
 			playGame();
 		}
 		onClick();
+		
 		function playGame(){
 			var choice;
 			humansTurn(choice);
@@ -52,7 +53,7 @@ var winner = new Array();
 							$(cpuRepick).html("O");
 							console.log("CPUs repick was successful");
 						if (cpuWinArray.includes(true)){
-							decideWinner(winner);
+							decideWinner(gameWinner);
 							isTaken = false;
 						}
 						isTaken = false;
@@ -77,15 +78,15 @@ var winner = new Array();
   			var draw = ((fullGrid === 9) && (!humanWins && !cpuWins));
   			if (humanWins) {
     			humanWin();
-    			winner.push("Human");
+    			gameWinner.push("Human");
     			return humanWins;
   			} else if (cpuWins) {
     			cpuWin();
-    			winner.push("CPU");
+    			gameWinner.push("CPU");
     			return cpuWins;
   			} else if (draw) {
     			catsGame();
-    			winner.push("Draw");
+    			gameWinner.push("Draw");
     			return draw;
   			} else {
     			console.log('game on...');
@@ -126,24 +127,35 @@ var winner = new Array();
 		function disableSpotsLeft() {
 			var notHumanOrCpu = document.querySelectorAll("div.gridItem:not(.blue):not(.red)");
   				$(notHumanOrCpu).addClass("gray");
-  				$(notHumanOrCpu).addClass("notClickable");
+				$(notHumanOrCpu).addClass("notClickable");
+				$('#resetButton').addClass("grayButton");
+				$('#resetButton').css("display","block");
   				return;
 			}
+		function reset(){
+			$('#resetButton').click(function(){
+				$('.gridItem').removeClass("blue red gray notClickable");
+				$('.gridItem').html("");
+				$('#winningHeader').css("display","none");
+				$('#resetButton').css("display","none");
+			});
+			onClick();
+		}
   		function addHuman(event, choice){
   			choice = '#' + event.currentTarget.id;
 					if ((humanPickedArray.includes(choice) == false) && (cpuPickedArray.includes(choice) == false)){	
-							if (winner[0] === undefined){
+							if (gameWinner[0] === undefined){
 								$(choice).addClass("blue");
 								$(choice).html("X");
 								humanPickedArray.push(choice);
 							} 
-							if (!winner.includes("Human") || !winner.includes("CPU") == false){
+							if (!gameWinner.includes("Human") || !gameWinner.includes("CPU") == false){
 								cpusTurn(choice);
 							}
-							decideWinner(winner);
+							decideWinner(gameWinner);
 					} else if (humanPickedArray.includes(choice) == true && cpuPickedArray.includes(choice) == true){
 							window.alert("Your choice has already been taken, please choose another.");
 					} else {	
-						console.log(choice);
+						// console.log(choice);
 					}
   		}	
